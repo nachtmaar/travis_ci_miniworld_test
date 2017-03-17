4a725c66b276dbf221d24c75611debdcfb8dfc98
Nils Schmidt <nightm4re@freenet.de>
2017-03-17 20:55:19 +0100

    update

--- test_kvm.py
+++ test_kvm.py
@@ -1,9 +1,13 @@
 import os
 import subprocess
+import time
+import signal
 
 def test_kvm():
     subprocess.check_call(["wget", "https://downloads.openwrt.org/chaos_calmer/15.05.1/x86/kvm_guest/openwrt-15.05.1-x86-kvm_guest-combined-ext4.img.gz"])
     subprocess.check_call(["gunzip", "openwrt-15.05.1-x86-kvm_guest-combined-ext4.img.gz"])
-    subprocess.check_call(["kvm", "--nographic", "openwrt-15.05.1-x86-kvm_guest-combined-ext4.img"])
+    proc = subprocess.Popen(["kvm", "--nographic", "openwrt-15.05.1-x86-kvm_guest-combined-ext4.img"])
+    time.sleep(10)
+    os.kill(proc.pid, signal.SIGINT)
 
     assert False
